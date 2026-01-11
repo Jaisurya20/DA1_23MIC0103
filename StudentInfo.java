f7b3210aad0871a79803549c43658ea799ac6c53
Jaisurya20 <jaisurya8105@gmail.com>
2026-01-11 22:31:48 +0530

    First commit - Java program

---
+++ StudentInfo.java
@@ -0,0 +1,7 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Student Name: Keerthana ");
+        System.out.println("Reg no Number: 23MIC0123");
+        System.out.println("Message: Jenkins GitHub Integration Successful");
+    }
+}
